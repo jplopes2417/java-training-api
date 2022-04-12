48be0f48efc5a7af8c1b1fdf80e4a0970889915f
Jonathan Lopes <jonathanlopes2417@gmail.com>
2022-04-11 21:13:50 -0300

    atualização testes controller

--- src/test/java/br/com/training/controller/UserControllerTest.java
+++ src/test/java/br/com/training/controller/UserControllerTest.java
@@ -2,12 +2,15 @@ package br.com.training.controller;
 
 import br.com.training.dto.UserRequestDto;
 import br.com.training.dto.UserResponseDto;
+import br.com.training.dto.UserUpdateDto;
 import br.com.training.mapper.UserMapper;
+import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
 import br.com.training.service.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,11 +27,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(UserController.class)
@@ -50,9 +54,15 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private URI uri;
+
+    @BeforeEach
+    public void setup() throws URISyntaxException {
+        uri = new URI("/users");
+    }
+
     @Test
     void criarUsuarioComSucessoViaController() throws Exception {
-        URI uri = new URI("/users");
 
         UserRequestDto userRequestDto = userRequestDtoValido();
         UserResponseDto userResponseDto = UserMapper.INSTANCE.toResponseDto(userRequestDto);
@@ -61,7 +71,7 @@ class UserControllerTest {
 
         jsonObject.put("name", userRequestDto.getName());
         jsonObject.put("cpf", userRequestDto.getCpf());
-        jsonObject.put("birthDate", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        jsonObject.put("birthDate", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")).toString());
         jsonObject.put("email", userRequestDto.getEmail());
 
         ResponseEntity responseEntity = new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
@@ -73,16 +83,9 @@ class UserControllerTest {
 
         when(userService.salvarUsuario(any(UserRequestDto.class))).thenReturn(responseEntity);
 
-        String teste = "{\n" +
-                "        \"name\": \"Teste User\",\n" +
-                "        \"email\": \"teste@teste.algo2\",\n" +
-                "        \"cpf\": \"172.118.470-85\",\n" +
-                "        \"birthDate\": \"2021-08-08\"\n" +
-                "}\n";
-
         mockMvc.perform(MockMvcRequestBuilders
                         .post(uri)
-                        .content(teste)
+                        .content(getUsuarioAsString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
     }
@@ -113,11 +116,38 @@ class UserControllerTest {
     }
 
     @Test
-    void deleteUser() {
+    void deleteUser() throws Exception {
+        URI uri = new URI("/users/1");
+
+        UserRequestDto userRequestDto = userRequestDtoValido();
+        UserResponseDto userResponseDto = UserMapper.INSTANCE.toResponseDto(userRequestDto);
+
+        ResponseEntity responseEntity = new ResponseEntity<>(null, HttpStatus.OK);
+
+        when(userService.deletarUsuario(any())).thenReturn(responseEntity);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(uri))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    void updateUser() {
+    void updateUser() throws Exception {
+        URI uri = new URI("/users/1");
+
+        ResponseEntity responseEntity = new ResponseEntity<>(null, HttpStatus.OK);
+
+//        when(userService.atualizarUsuario(anyInt(), any())).thenReturn(responseEntity);
+        when(userService.atualizarUsuario(anyLong(), any(UserUpdateDto.class))).thenReturn(responseEntity);
+
+        String usuario = "{" +
+                "        \"name\": \"Teste User\",\n" +
+                "        \"email\": \"teste@teste.algo2\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put(uri)
+                        .content(usuario)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     // TODO: PENSAR NUMA FORMA DE EXTRAIR ESSE MÉTODO
@@ -134,5 +164,14 @@ class UserControllerTest {
         assertEquals(userResponseDto.getEmail(), userResponseRetorno.getEmail());
         assertEquals(userResponseDto.getName(), userResponseRetorno.getName());
         assertEquals(userResponseDto.getBirthDate(), userResponseRetorno.getBirthDate());
+    }
+
+    private String getUsuarioAsString() {
+        return "{\n" +
+                "        \"name\": \"Teste User\",\n" +
+                "        \"email\": \"teste@teste.algo2\",\n" +
+                "        \"cpf\": \"172.118.470-85\",\n" +
+                "        \"birthDate\": \"2021-08-08\"\n" +
+                "}\n";
     }
 }
